958ae9e01eb1720b13fcd15bb1ec6ca6d03812bc
lode <rioaformyking@gmail.com>
2022-05-06 11:00:13 +0800

    add:cacheTable

---
+++ cacheTable.go
@@ -0,0 +1,30 @@
+package cache4go
+
+import (
+	"log"
+	"sync"
+)
+
+type CacheTable struct {
+	//seft lock
+	sync.RWMutex
+
+	//table name
+	name string
+	//table's items
+	items map[interface{}]*CacheItem
+
+	loger *log.Logger
+}
+
+//table's name
+func (table *CacheTable) Name() string {
+	return table.name
+}
+
+//get table's item count
+func (table *CacheTable) Count() int {
+	table.RLock()
+	defer table.RUnlock()
+	return len(table.items)
+}
